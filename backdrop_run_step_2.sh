71bb9044e95b668bfaea0f0380ce3126cfd91539
francis <francis@fsoft.nnet>
2022-03-28 17:53:01 +0000

    corrected typo in backdrop_run_step_2.sh

--- backdrop_run_step_2.sh
+++ backdrop_run_step_2.sh
@@ -3,7 +3,7 @@
 # Script to run Step 2 in Backdrop Upgrade
 #
 SECONDS=0
-ansible-playbook ~/ansible/backdrop_upgrade_made_easy/lpaybooks/backdrop_step_2.yaml -v
+ansible-playbook ~/ansible/backdrop_upgrade_made_easy/playbooks/backdrop_step_2.yaml -v
 ELAPSED="Elapsed: $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
 echo "${ELAPSED}"
 
